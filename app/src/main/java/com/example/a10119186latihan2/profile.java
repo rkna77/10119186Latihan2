4a71d23e05a55c5d7537c89056ac424fe11f11ee
rkna77 <rikanuradila575@gmail.com>
2022-04-26 14:18:18 +0700

    profile

--- app/src/main/java/com/example/a10119186latihan2/profile.java
+++ app/src/main/java/com/example/a10119186latihan2/profile.java
@@ -1,4 +1,14 @@
 package com.example.a10119186latihan2;
 
-public class profile {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profile extends AppCompatActivity {
+    //Rika Nur'adila IF5 10119186, 25 April 2022//
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
 }
